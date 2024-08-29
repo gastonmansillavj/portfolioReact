9d18045e38bab75a8c9dc3a13ec48cda7ae1eba5
gastonmansillavj <gastonmansilla.vj@gmail.com>
2024-08-29 14:45:03 -0300

    build doc

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: 'docs',  // Configura la carpeta de salida de la compilación
+    rollupOptions: {
+      // Asegúrate de que la opción base esté configurada correctamente si estás usando GitHub Pages
+      input: {
+        main: 'index.html'
+      },
+      output: {
+        // Puedes personalizar la salida aquí si es necesario
+      }
+    }
+  }
 })
